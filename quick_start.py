294262e317dbf224fc5f730963cc151a52bbc3c6
Jack Sanchez <evo@gms.tku.edu.tw>
2020-06-29 19:40:39 +0800

    init

---
+++ quick_start.py
@@ -0,0 +1,31 @@
+""" Quickstart script for InstaPy usage """
+# imports
+from instapy import InstaPy
+from instapy import smart_run
+import os
+
+# login credentials
+insta_username = os.getenv("IG_USER")  # <- enter username here
+insta_password = os.getenv("IG_PW")  # <- enter password here
+
+# get an InstaPy session!
+# set headless_browser=True to run InstaPy in the background
+session = InstaPy(username=insta_username,
+                  password=insta_password,
+                  headless_browser=False,
+                  )
+
+with smart_run(session):
+    """ Activity flow """
+    # general settings
+    session.set_relationship_bounds(enabled=True,
+                                    delimit_by_numbers=True,
+                                    max_followers=4590,
+                                    min_followers=100,
+                                    min_following=77)
+
+    session.set_dont_include(["friend1", "friend2", "friend3"])
+    session.set_dont_like(["pizza", "#store"])
+
+    # activity
+    session.like_by_tags(["facefilters"], amount=10)
